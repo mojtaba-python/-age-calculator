89830aa1ad1d9586fab60b4fa90f73faf1683e90
moji <mojtaba@klaie@gmail.com>
2024-08-10 09:13:49 +0330

    update css

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var result = document.getElementById('result');
-
+var box = document.getElementById('box');
 // function getDate(d1,d2) {
-    
+
 //     const time = Math.abs(d1-d2);
 //     const date =  new Date(time);
 
@@ -24,24 +24,27 @@ var result = document.getElementById('result');
 // 
 
 function getDate() {
-    var birth =  document.getElementById('input').value;
-    var d1 = new Date();
-    var d2 = new Date(birth);
+  var birth = document.getElementById('input').value;
+  var d1 = new Date();
+  var d2 = new Date(birth);
 
-    const time = Math.abs(d1-d2);
-    const date =  new Date(time);
+  const time = Math.abs(d1 - d2);
+  const date = new Date(time);
 
-    const year = date.getUTCFullYear() - 1970;
-    const months = date.getUTCMonth();
-    const day = date.getUTCDate() -1;
-    if(birth){
-		result.style.cssText="color:#fff;background-color:#2c353f;padding:1rem;border:1px solid green";
-		
-		result.innerHTML= "سال: " + year + "  ماه: " + months + "  روز: " + day;
-	}else{
-		result.style.cssText="color:#fff;background-color:#2c353f;padding:1rem;border:1px solid red";
-        result.innerHTML= "لطفا تاریخ تولد خود وارد کنید ";
-	}
+  const year = date.getUTCFullYear() - 1970;
+  const months = date.getUTCMonth();
+  const day = date.getUTCDate() - 1;
+
+  if (birth) {
+    box.style.cssText = "border: 1px solid green;"
+    result.style.cssText = "color: green; background-color: rgb(0 0 0 / 35%); padding: 1rem; border: 1px solid green;";
+
+    result.innerHTML = "سال: " + year + "  ماه: " + months + "  روز: " + day;
+  } else {
+    box.style.cssText = "border: 1px solid #b90606;"
+    result.style.cssText = "color: rgb(165 19 19); background-color: rgb(0 0 0 / 35%); padding: 1rem; border: 1px solid #860f0f;";
+    result.innerHTML = "لطفا تاریخ تولد خود وارد کنید ";
+  }
 }
 
 
